427852b6e0474dc2f1b65a5ac736f7349921e512
AinaKhanDurrani <ainakhan464@gmail.com>
2023-10-17 17:12:36 +0500

    temp-converter

---
+++ script.mjs
@@ -0,0 +1,32 @@
+let fahInput = document.getElementById("fah");
+let celInput = document.getElementById("cel");
+
+
+function convert() {
+    if (celInput.value !== "") {
+        to_fahrenheit();
+    } else if (fahInput.value !== "") {
+        to_celsius();
+    }
+}
+
+function to_fahrenheit() {
+    let celsius = parseFloat(celInput.value);
+    let fahrenheit = (celsius * 9/5) + 32;
+    fah.value = fahrenheit.toFixed(2) +"°F";
+}
+
+function to_celsius() {
+    let fahrenheit = parseFloat(fahInput.value);
+    let celsius = (fahrenheit - 32) * 5/9;
+    cel.value = celsius.toFixed(2)+"°C";
+}
+function clearIfEmpty(currentFieldId, otherFieldId) {
+    var currentField = document.getElementById(currentFieldId);
+    var otherField = document.getElementById(otherFieldId);
+
+    if (currentField.value === "") {
+        otherField.value = "";
+        document.getElementsByClassName("result").innerHTML = " ";
+    }
+}
